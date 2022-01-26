50b7e76866b3f567e59d814cd92ee73f07827aa4
Syed Mustajab Abbas Nqvi <97434376+Mustajab-abbas@users.noreply.github.com>
2022-01-26 13:59:25 +0500

    Ido_Hci

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import './assets/css/style.css'
 import './assets/css/responsive.css';
 import Header from './components/Header';
+import DashboardHeader from './components/DashboadHeader';
 import Footer from './components/Footer';
 import Home from './screens/Home'
 import IdoProjects from './screens/Ido_projects'
+import HciProjects from "./screens/Hci_projects"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEagerConnect, useInactiveListener } from './hooks/useEagerConnect';
@@ -17,7 +19,8 @@ function App() {
     <Router>
         <Routes>
         <Route path="/" element={<Home header={<Header/>} footer={<Footer/>}/>}  />
-        <Route path="/Ido-Projects" element={<IdoProjects header={<Header/>}/>}  />
+        <Route path="/ido-projects" element={<IdoProjects header={<DashboardHeader/>} footer={<Footer/>} />}  />
+        <Route path="/hci-projects" element={<HciProjects header={<DashboardHeader/>} footer={<Footer/>}/>}  />
         </Routes>
     </Router>
   );
