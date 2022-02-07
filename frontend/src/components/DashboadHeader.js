029e0001f9e854ff758f3276b98a33a5115453a8
MutahirPluto <mutahir.pluton@gmail.com>
2022-02-07 19:05:39 +0500

    Updated

--- frontend/src/components/DashboadHeader.js
+++ frontend/src/components/DashboadHeader.js
@@ -136,25 +136,62 @@ function DashboardHeader(props){
   //   return new ethers.providers.Web3Provider(connection)
   // };
 
-  
+  // loadProvider()
 
+
+  // export default function wallet_model() {
+  //   const [loading, setLoading] = useState(false);
+  //   return {
+  //   get web3Loading() {
+  //   return loading
+  //   },
+  //   async getweb3() {
+  //   setLoading(true);
+  //   let web3Modal;
+  //   let provider;
+  //   let web3;
+  //   let providerOptions;
+  //   providerOptions = {
+  //   metamask: {
+  //   id: "injected",
+  //   name: "MetaMask",
+  //   type: "injected",
+  //   check: "isMetaMask",
+  //   },
+  //   walletconnect: {
+  //   package: WalletConnectProvider, // required
+  //   options: {
+  //   infuraId: "INFURA_ID", // Required
+  //   network: "rinkeby",
+  //   qrcodeModalOptions: {
+  //   mobileLinks: [
+  //   "rainbow",
+  //   "metamask",
+  //   "argent",
+  //   "trust",
+  //   "imtoken",
+  //   "pillar"
+  //   ]
+  //   }
+  //   }
+  //   },
+  //   authereum: {
+  //   package: Authereum // required
+  //   },
+  //   };
+  //   web3Modal = new Web3Modal({
+  //   network: "rinkeby",
+  //   cacheProvider: true,
+  //   providerOptions
+  //   });
+  //   provider = await web3Modal.connect();
  
-      
-    
-
-    //   async function onConnect() {
-    //     try {
-    //       let provider = await web3Modal.connect();
-    //       // const hello = new walletconnect()
-    //       console.log("provider", provider)
-    //     } catch (e) {
-    //         console.log(e)
-    //         return;
-    //   }
-    // }
-
-      
-    
+  //   web3 = new Web3(provider);
+  //   setLoading(false);
+  //   return web3;
+  //   },
+  //   }
+  //   }
 
     return (
         <>
